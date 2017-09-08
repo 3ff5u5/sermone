e2bd28f583bf640ab35ae6ba81c06b9eeef127b5
3ff5u5 <effsus@gmail.com>
2017-09-08 21:28:16 +0300

    Initial commit

---
+++ Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sermone
+{
+    class Dictionary
+    {
+        private int _wordsCount;
+        private string[] _wordsOriginal;
+        private string[] _wordsTranslated;
+
+        public Dictionary(int count, string[] original, string[] translated)
+        {
+            wordsCount = count;
+            _wordsOriginal = original;
+            _wordsTranslated = translated;
+        }
+        public int wordsCount
+        {
+            get { return _wordsCount; }
+            set { _wordsCount = value; }
+        }
+
+        public string getWordOriginal(int IDX)
+        {
+            return _wordsOriginal[IDX];
+        }
+
+        public string getWordTranslated(int IDX)
+        {
+            return _wordsTranslated[IDX];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace Sermone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_DICT_SIZE = 100;
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            List<Dictionary> dictionaries = new List<Dictionary>();
+            
+            if (Directory.Exists(path + "dict"))
+            {
+                foreach(string dictPath in Directory.GetFiles(path + "dict"))
+                {
+                    string[] original = new string[MAX_DICT_SIZE];
+                    string[] translated = new string[MAX_DICT_SIZE];
+
+                    
+                    using (StreamReader sr = new StreamReader(dictPath))
+                    {
+                        int count = 0;
+                        count++;
+                        while (true)
+                        {
+                            string temp = sr.ReadLine();
+                            if (temp == null)
+                            {
+                                dictionaries.Add(new Dictionary(count, original, translated));
+                                break;
+                            }
+                            string[] tempArr = temp.Split(':');
+                            original[count] = tempArr[0];
+                            translated[count] = tempArr[1];
+                            count++;                    
+                        }
+                    }
+                }
+            }
+            //demo
+            while(true)
+            {
+                Random random = new Random();
+                Dictionary dict = dictionaries[0];
+                int rndWordIDX = random.Next(0, dict.wordsCount);
+                string tempOriginalWord = dict.getWordOriginal(rndWordIDX);
+
+                Console.WriteLine(dict.getWordTranslated(rndWordIDX));
+                Console.WriteLine(Console.ReadLine() == tempOriginalWord);
+                Console.WriteLine("================================================");
+
+            }
+            //
+            Console.ReadLine();
+        }
+    }
+}
